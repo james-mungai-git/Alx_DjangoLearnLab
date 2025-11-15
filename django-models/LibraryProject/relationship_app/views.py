49fd213904dbc1027c7ddacf0c4a4676751f2d6c
james-mungai-git <j.murigi.mungai@gmail.com>
2025-11-15 20:08:57 +0300

    addjdjdjdjdjd

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,6 @@
 # relationship_app/views.py
 from django.shortcuts import render
-from django.views.generic import DetailView
+from django.views.generic.detail import DetailView
 from .models import Book, Library
 from .models import Library
 

30578edf727d05a99e4fb9f6a31707a8fd22d9a9
james-mungai-git <j.murigi.mungai@gmail.com>
2025-11-16 20:42:42 +0300

    made changes to admin

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,48 +1,28 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import Usermodel
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import CustomUser
 
-class CustomUserCreationForm(UserCreationForm):
-    class Meta(UserCreationForm.Meta):
-        model = Usermodel
-        fields = ("email", "username", "date_of_birth", "profile_photo")
-
-
-class CustomUserChangeForm(UserChangeForm):
-    class Meta:
-        model = Usermodel
-        fields = ("email", "username", "date_of_birth", "profile_photo",
-                  "is_active", "is_staff", "is_superuser")
-
-
+# Custom UserAdmin to handle additional fields
 class CustomUserAdmin(UserAdmin):
-    add_form = CustomUserCreationForm
-    form = CustomUserChangeForm
-    model = Usermodel
-
-    list_display = ("email", "username", "date_of_birth", "is_staff", "is_active")
-    list_filter = ("is_staff", "is_active")
+    model = CustomUser
+    list_display = ('email', 'username', 'date_of_birth', 'is_staff', 'is_active')
+    list_filter = ('is_staff', 'is_active')
 
     fieldsets = (
-        (None, {"fields": ("email", "username", "password")}),
-        ("Personal Info", {"fields": ("date_of_birth", "profile_photo")}),
-        ("Permissions", {"fields": (
-            "is_active", "is_staff", "is_superuser", "groups", "user_permissions"
-        )}),
+        (None, {'fields': ('username', 'email', 'password')}),
+        ('Personal Info', {'fields': ('date_of_birth', 'profile_photo')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active', 'is_superuser', 'groups', 'user_permissions')}),
     )
 
     add_fieldsets = (
         (None, {
-            "classes": ("wide",),
-            "fields": ("email", "username", "date_of_birth", "profile_photo",
-                       "password1", "password2", "is_staff", "is_active")}
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2', 'is_staff', 'is_active')}
         ),
     )
 
-    search_fields = ("email", "username")
-    ordering = ("email",)
+    search_fields = ('email', 'username')
+    ordering = ('email',)
 
-
-admin.site.register(Usermodel, CustomUserAdmin)
+# Register the custom user model with the admin
+admin.site.register(CustomUser, CustomUserAdmin)

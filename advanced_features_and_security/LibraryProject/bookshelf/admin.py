71c7585dfc7f3bf11339e14ea33d56ac1c8fd95d
james-mungai-git <j.murigi.mungai@gmail.com>
2025-11-16 20:38:03 +0300

    added the line in admin

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import Usermodel
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
-
+from .models import CustomUser
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):

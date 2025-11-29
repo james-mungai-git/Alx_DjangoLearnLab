1ad6036d4ea8b3a38bc5e49576e454c1ded7768c
james-mungai-git <j.murigi.mungai@gmail.com>
2025-11-29 17:57:06 +0300

    made changes

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import BookViewSet
-from .views import BookList
+from .views import BookList, BookViewSet
 
 router = DefaultRouter()
-router.register(r'books', BookViewSet)
+router.register(r'books_all', BookViewSet, basename='books_all')
 
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),
--- api_project/api_project/urls.py
+++ api_project/api_project/urls.py
@@ -1,13 +1,34 @@
-# api_project/urls.py
-
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework.authtoken.views import obtain_auth_token
-
-path('api/token/', obtain_auth_token),
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),   # This makes /api/books/ available globally
+    path('api/', include('api.urls')),
+
+
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+from django.contrib import admin
+from django.urls import path, include
+
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('api.urls')),
 ]

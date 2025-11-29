7b5ed15e11ccaed994962c39568c5a7cee8bfc71
james-mungai-git <j.murigi.mungai@gmail.com>
2025-11-29 14:31:45 +0300

    replaced simple list with CRUD operations

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -3,11 +3,9 @@ from .models import Book
 from .serializers import BookSerializer
 from rest_framework import viewsets
 
-class BookList(generics.ListAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
+
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    
+
     

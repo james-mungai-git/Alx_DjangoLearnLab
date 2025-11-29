087058e2e07d51e19ad1c134d1d570989b1d1746
james-mungai-git <j.murigi.mungai@gmail.com>
2025-11-29 17:40:59 +0300

    add authentication and permission layers to your existing API endpoints

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -2,10 +2,18 @@ from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
 
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
-    
+class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
--- api_project/api_project/urls.py
+++ api_project/api_project/urls.py
@@ -2,6 +2,10 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.authtoken.views import obtain_auth_token
+
+path('api/token/', obtain_auth_token),
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),

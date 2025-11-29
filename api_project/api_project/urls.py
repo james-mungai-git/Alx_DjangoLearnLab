cff681d1c0e9dc0eef217d79ca574ed462b94e97
james-mungai-git <j.murigi.mungai@gmail.com>
2025-11-29 18:30:03 +0300

    Made changes

--- api_project/api_project/urls.py
+++ api_project/api_project/urls.py
@@ -22,13 +22,3 @@ urlpatterns = [
 
 
 
-
-from django.contrib import admin
-from django.urls import path, include
-
-
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),
-]

0d6cffc113e9f98c7c7973a5f9c8d8b1ba09d389
James Murigi <j.murigi.mungai@gmail.com>
2025-12-07 16:53:18 +0300

    added CRUD operations to views

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,14 +1,30 @@
 from django.urls import path
-from . import views
-from django.contrib.auth.views import LogoutView
+from .views import (
+    PostListView,
+    PostDetailView,
+    PostCreateView,
+    PostUpdateView,
+    PostDeleteView,
+    profile,
+    register,
+    logout_user
+)
 from django.contrib.auth.views import LoginView
+from . import views
+
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('home/', views.home, name='home'),
-    path('profile/', views.profile, name='profile'),
+    path('home/', PostListView.as_view(), name='home'),                
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'), 
+    path('post/new/', PostCreateView.as_view(), name='post-create'),      
+    path('post/<int:pk>/edit/', PostUpdateView.as_view(), name='post-update'),  
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'), 
+
+    path('profile/', profile, name='profile'),
     path('login/', LoginView.as_view(template_name='blog/login.html'), name='login'),
-    path("logout/", views.logout_user, name="logout"),
-    path('register/', views.register, name='register'),
-    path('blog_post/', views.blog_post, name = 'blog')
+    path('logout/', logout_user, name='logout'),
+    path('register/', register, name='register'),
+    
+    
 ]
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,48 +1,87 @@
 from django.shortcuts import render, redirect
-from .forms import Register, PostForms
 from django.contrib import messages
 from django.contrib.auth import login, logout, authenticate
-from django.contrib.auth.decorators  import login_required
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .forms import Register, PostForms
 from .models import Post
+from django.core.exceptions import PermissionDenied
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 
-@login_required(login_url = '/login/')
+class PostListView(ListView):
+    model = Post
+    template_name = "blog/home.html"
+    context_object_name = "posts"
+    ordering = ['-published_date']  
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = "blog/post_detail.html"
+    context_object_name = "post"
+
+class PostCreateView(CreateView):
+    model = Post  
+    form_class = PostForms  
+    template_name = "blog/blog_post.html"
+    success_url = reverse_lazy('home')  
+    
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(UpdateView):
+    model = Post
+    form_class = PostForms  
+    template_name = "blog/blog_post.html"
+    success_url = reverse_lazy('home')
+    
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+    template_name = "blog/blog_post.html"   
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+ 
+
+class PostDeleteView(LoginRequiredMixin, DeleteView):
+    model = Post
+    template_name = 'blog/post_confirm_delete.html'  
+    success_url = reverse_lazy('home')
+
+    def get_object(self):
+        obj = super().get_object()
+        if obj.author != self.request.user:
+            raise PermissionDenied()
+        return obj
+
+@login_required(login_url='/login/')
+
 
 def home(request):
-    posts = Post.objects.all()  
+    posts = Post.objects.all().order_by('published_date')  
     
     if request.method == "POST":
         post_id = request.POST.get("post_id")
-        print(post_id)   
         Post.objects.filter(id=post_id, author=request.user).delete()
+        messages.success(request, "Post deleted successfully!")
         return redirect("home")
 
     return render(request, "blog/home.html", {"posts": posts})
 
 def profile(request):
-    pass   
-
-def blog_post(request):
-    if request.method == "POST":
-        form = PostForms(request.POST)
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.author = request.user
-            post.save()
-            return redirect("home")
-    else:
-        form = PostForms()
-
-    return render(request, "blog/blog_post.html", {"form": form})
-
+    return render(request, "blog/profile.html")
 
 def register(request):
     if request.method == "POST":
         form = Register(request.POST)
         if form.is_valid():
             user = form.save()
-            messages.success(request, "Account created successfully")
+            messages.success(request, "Account created successfully!")
             return redirect("login")
     else:
         form = Register()
@@ -51,4 +90,5 @@ def register(request):
 
 def logout_user(request):
     logout(request)
+    messages.success(request, "Logged out successfully!")
     return redirect("/login/")

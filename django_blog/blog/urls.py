1927ed05582c4ebe1b8c21cdafb7236ac696e699
james-mungai-git <j.murigi.mungai@gmail.com>
2025-12-07 16:55:33 +0300

    Change URL pattern for post update route

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -18,7 +18,7 @@ urlpatterns = [
     path('home/', PostListView.as_view(), name='home'),                
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'), 
     path('post/new/', PostCreateView.as_view(), name='post-create'),      
-    path('post/<int:pk>/edit/', PostUpdateView.as_view(), name='post-update'),  
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),  
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'), 
 
     path('profile/', profile, name='profile'),

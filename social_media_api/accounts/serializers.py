11df0b36b6404e17fda244aaf986b240fb5ac66b
James Murigi <j.murigi.mungai@gmail.com>
2025-12-15 00:01:03 +0300

    jhjjhjhhj

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -13,9 +13,18 @@ class UserSerializer(serializers.ModelSerializer):
         extra_kwargs = {'password': {'write_only': True}}
     
     def create(self, validated_data):
-        user = User.objects.create_user(**validated_data)
+        password = validated_data.pop('password')
+        user = User.objects.create_user(
+            username=validated_data.get('username'),
+            email=validated_data.get('email'),
+            password=password
+        )
+        # assign optional fields
+        user.bio = validated_data.get('bio', '')
+        user.profile_picture = validated_data.get('profile_picture', None)
+        user.save()
         return user
-
+    
 class LoginSerializer(serializers.Serializer):
     username = serializers.CharField()
     password = serializers.CharField(write_only=True)

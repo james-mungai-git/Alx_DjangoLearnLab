feaf419c4f80d64333d0d1f51f54604e9ff378f1
James Murigi <j.murigi.mungai@gmail.com>
2025-12-17 12:02:19 +0300

    configured urls

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -2,12 +2,12 @@ from django.urls import path
 from .views import PostViewSet, CommentViewSet
 
 urlpatterns = [
-    path('create-post/',PostViewSet.as_view({'get': 'list', 
+    path('api/create-post/',PostViewSet.as_view({'get': 'list', 
                                              'put':  'update',
                                              'post': 'create'}),
          name='create-post'),
     
-    path('create-post/',CommentViewSet.as_view({'get': 'list', 
+    path('api/post-comment/',CommentViewSet.as_view({'get': 'list', 
                                                 'put':  'update',
                                                 'post': 'create'}),
          name='create-post'),

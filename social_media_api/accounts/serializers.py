dc5077d399306939beb2a7ba5e212696d0355f2f
James Murigi <j.murigi.mungai@gmail.com>
2025-12-22 08:00:54 +0300

    serializer.py

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -5,7 +5,7 @@ from django.contrib.auth import get_user_model
 from rest_framework.authtoken.models import Token
 
 
-User = get_user_model()
+User = get_user_model().object.create_user
 
      
 # Simple registration
